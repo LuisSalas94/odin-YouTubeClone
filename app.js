dcf89dcaa2e594dcdf76d6ae5136cc5ef04dfad9
Fernando <luisfernandosalasg@gmail.com>
2021-12-19 12:08:12 -0500

    add media queries

--- app.js
+++ app.js
@@ -4,3 +4,17 @@ const btn = document.querySelector("#btn-menu");
 btn.addEventListener("click", () => {
 	container.classList.toggle("active");
 });
+
+const checkSize = () => {
+	if (window.innerWidth <= 768) {
+		container.classList.remove("active");
+	} else {
+		container.classList.add("active");
+	}
+};
+
+checkSize();
+
+window.addEventListener("resize", () => {
+	checkSize();
+});
